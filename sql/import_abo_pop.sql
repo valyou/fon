1f96d6760984eaebf1947130c2d676f27c73ec43
Adrian Smith <fon@thefon.net>
2015-07-04 21:34:41 +0800

    import and process abo pop

---
+++ sql/import_abo_pop.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS raw_abo_pop;
+CREATE TABLE raw_abo_pop (
+"STATE" integer,
+"State" text,
+"REGIONTYPE" text,
+"Region Type" text,
+"FREQUENCY" text,
+"Frequency" text,
+"MEASURE" integer,
+"Sex" text,
+"AGE" text,
+"Age" text,
+"TIME" integer,
+"Time" integer,
+"REGION" varchar(9),
+"Region" text,
+"INGP" varchar(3),
+"Indigenous Status" text,
+Value integer,
+Flags text
+);
+\COPY raw_abo_pop FROM 'ABS_CENSUS2011_B07_Data_40fa835b-00f3-4993-b534-fdd14abf4ddf.csv' DELIMITERS ',' WITH CSV HEADER
