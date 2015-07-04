2705ccdf91e82cfe00883854ad6bb84a27942617
Adrian Smith <fon@thefon.net>
2015-07-04 21:34:29 +0800

    import and process abo pop

--- scripts/import_shp.sh
+++ scripts/import_shp.sh
@@ -10,13 +10,9 @@
 # Run from data directory, expects subdirs 'shp' and 'sql'
 
 shp2pgsql -s 4326 -d shp/abo_usage/ALT_Estate_20150703_GovHack_NonexclusiveAgreement.shp raw_abo_usage > sql/raw_abo_usage.sql
-psql valyou < sql/raw_abo_usage.sql
 
 shp2pgsql -s 4326 -d shp/abo_comms/Remote_Aboriginal_Comms_and_Town_Based_Reserves_GovHack_NonexclusiveAgreement.shp raw_abo_comms > sql/raw_abo_comms.sql
-psql valyou < sql/raw_abo_comms.sql
 
 shp2pgsql -s 4326 -d shp/abo_places/place.shp raw_abo_places > sql/raw_abo_places.sql
-psql valyou < sql/raw_abo_usage.sql
 
 shp2pgsql -s 4326 -d -W LATIN1 shp/abo_title/NTD_Register_Nat.shp raw_abo_title > sql/raw_abo_title.sql
-psql valyou < sql/raw_abo_title.sql
